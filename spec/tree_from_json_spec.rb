3b2eb7bd9ccc54070f21d69bb0426d5d61308522
Jeroen Bos <Jeroenbos90@gmail.com>
2024-06-18 12:03:05 +0200

    Hero image and layout option

--- spec/tree_from_json_spec.rb
+++ spec/tree_from_json_spec.rb
@@ -10,7 +10,7 @@ describe Nutmeg::TreeFromJson do
   describe "#initialize" do
     it "verifies initialize works correctly" do
       expect(@tree_from_json.tree.class).to eq(Nutmeg::TagTree)
-      expect(([:tag_id, :tag, :slug, :name, :tag_image, :tag_image_file_name] - @tree_from_json.tree.original.each_leaf.first.content.keys).empty?).to eq(true)
+      expect(([:tag_id, :tag, :slug, :name, :layout, :tag_image, :tag_image_file_name, :hero_image, :hero_image_file_name] - @tree_from_json.tree.original.each_leaf.first.content.keys).empty?).to eq(true)
     end
   end
 end
